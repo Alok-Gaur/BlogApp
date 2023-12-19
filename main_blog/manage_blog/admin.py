7e931958535dc4233efa1ae7c0f640a148c2b479
Alok-Gaur <alokgaur200@gmail.com>
2023-12-19 16:39:55 +0530

    updated admin page and truncate the content

--- main_blog/manage_blog/admin.py
+++ main_blog/manage_blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import UserPost
+from django.utils.text import Truncator
 
 # Register your models here.
-admin.site.register(UserPost)
+class UserPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_short_content')
+
+    def display_short_content(self, obj):
+        return Truncator(obj.content).words(16, html=True)
+    display_short_content.short_discription = 'content'
+admin.site.register(UserPost, UserPostAdmin)
