252606053e1175e435505b9bda561ac54c9006ea
Alok-Gaur <alokgaur200@gmail.com>
2023-12-19 16:09:18 +0530

    syntex corrected

--- main_blog/manage_blog/models.py
+++ main_blog/manage_blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from
+
 
 # Create your models here.
 class UserPost(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
-    timestamp = models.DateTimefield(auto_now=True)
+    timestamp = models.DateTimeField(auto_now=True)
