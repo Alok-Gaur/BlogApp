db24456fa85d70f0aeb7f140eb3809ce47cca323
Alok-Gaur <alokgaur200@gmail.com>
2023-12-19 17:13:16 +0530

    connected landing page to database

--- main_blog/manage_blog/views.py
+++ main_blog/manage_blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import UserPost
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    blog_posts = UserPost.objects.all()[::-1]
+    return render(request, 'index.html', {'blog_posts': blog_posts})
