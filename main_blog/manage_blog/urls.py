498135b29e40cdbcc513f13b0e4965290c941874
Alok-Gaur <alokgaur200@gmail.com>
2023-12-19 18:05:21 +0530

    url and view created for blog-page

--- main_blog/manage_blog/urls.py
+++ main_blog/manage_blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path("/<int:key>", views.blog_page, name="blog_page")
 ]
--- main_blog/manage_blog/views.py
+++ main_blog/manage_blog/views.py
@@ -5,3 +5,8 @@ from .models import UserPost
 def index(request):
     blog_posts = UserPost.objects.all()[::-1]
     return render(request, 'index.html', {'blog_posts': blog_posts})
+
+
+def blog_page(request, key):
+    blog_post = UserPost.objects.all().filter(id=key)
+    return render(request, 'blog-page.html', {'blog_post': blog_post})
