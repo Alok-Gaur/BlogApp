b23d2f308e6475911fea7be647b23d27b1a698d2
Alok-Gaur <alokgaur200@gmail.com>
2023-12-20 23:27:34 +0530

    linked with admin page and basic CURD operation

--- main_blog/manage_blog/urls.py
+++ main_blog/manage_blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path("/<int:key>", views.blog_page, name="blog_page")
+    path("/<int:key>", views.blog_page, name="blog_page"),
+    path("admin/", views.admin_page, name="admin_page")
 ]
--- main_blog/manage_blog/views.py
+++ main_blog/manage_blog/views.py
@@ -10,3 +10,20 @@ def index(request):
 def blog_page(request, key):
     blog_post = UserPost.objects.get(id = key)
     return render(request, 'blog-page.html', {'blog_post': blog_post})
+
+
+def admin_page(request):
+    if request.method == 'POST':
+        # this checks for which form user submitted
+        if request.POST.get('form_type') == 'create':
+            title = request.POST['title']
+            content = request.POST['input-content']
+            UserPost.objects.create(title=title, content=content)
+        
+        else:
+            id = request.POST['blog_id']
+            post = UserPost.objects.get(id=id)
+            post.delete()
+
+    blog_posts = UserPost.objects.all().order_by('timestamp')
+    return render(request, 'admin-page.html', {'blog_posts': blog_posts})
